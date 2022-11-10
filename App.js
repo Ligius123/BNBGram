8c12dd2a6aab48e2a19ae86bc71c2bf43e590f22
Ligius Matesoni <ligius.matesoni@student.upt.ro>
2022-11-10 22:53:25 +0200

    Adding some new styles

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
+import { StyleSheet, ImageBackground } from "react-native";
 
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
-import WelcomeScreen from "./screens/WelcomeScreen";
 import AddPlace from "./screens/AddPlace";
 import AllPlaces from "./screens/AllPlaces";
 import IconButton from "./components/ui/IconButton";
@@ -39,20 +39,6 @@ function AuthenticatedStack() {
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
-      <Stack.Screen
-        name="Welcome"
-        component={WelcomeScreen}
-        options={{
-          headerRight: ({ tintColor }) => (
-            <IconButton
-              icon="exit"
-              color={tintColor}
-              size={24}
-              onPress={authCtx.logout}
-            />
-          ),
-        }}
-      />
       <Stack.Screen
         name="AllPlaces"
         component={AllPlaces}
@@ -108,3 +94,9 @@ export default function App() {
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
