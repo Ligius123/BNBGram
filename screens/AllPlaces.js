9730bc114b9fa04d4f4472e7455a1adca82fcce6
Ligius Matesoni <ligius.matesoni@student.upt.ro>
2022-11-29 16:30:56 +0200

    Outputting a list of all places

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
+import { useIsFocused } from "@react-navigation/native";
+
 import PlacesList from "../components/Places/PlacesList";
 import BackgroundImage from "../components/ui/BackgroundImage";
 
-function AllPlaces() {
+function AllPlaces({ route }) {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    }
+  }, [isFocused, route]);
+
   return (
     <BackgroundImage>
-      <PlacesList />
+      <PlacesList places={loadedPlaces} />
     </BackgroundImage>
   );
 }
